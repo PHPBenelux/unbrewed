5f0f5addbb0fdfd0abb25992c01cf39b0aaed535
Matthew Weier O'Phinney <matthew@zend.com>
2012-10-24 22:50:09 -0700

    One more time

--- src/app.php
+++ src/app.php
@@ -18,4 +18,13 @@ $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
     return $twig;
 }));
 
+$mongo = new Mongo('mongodb://127.11.221.129:27017/', array(
+    'connect' => true,
+    'username' => 'admin',
+    'password' => 'WSvyUln6D5Fu',
+));
+$db = new MongoDB($mongo, 'unbrewed');
+$collection = new MongoCollection($db, 'unbrewd');
+$app['mongo'] = $collection;
+
 return $app;
